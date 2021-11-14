f98f4e2e2f4a6dd0c8aebf0bc2b3880dc452c966
dezoliveira <andresoliveira@protonmail.com>
2021-11-14 19:13:51 -0300

    Projeto 3 + Projeto 4

---
+++ Project 3 - Todo List - Initial/scripts.js
@@ -0,0 +1,34 @@
+var todos = []
+
+const todosApp = {
+  data(){
+    return {
+      todos: window.todos,
+      newTodo: {
+        done: false
+      }
+    }
+  },
+  methods: {
+    addTodo: function() {
+      if(this.newTodo.text){
+      this.todos.push(this.newTodo)
+      this.newTodo = {
+        done: false
+      }
+      localStorage.setItem("todos", JSON.stringify(this.todos))
+      }else{
+        alert('Texto obrigatório!!!')
+      }
+    }
+  },
+  created(){
+    this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")): this.todos
+  },
+  updated(){  
+    localStorage.setItem("todos", JSON.stringify(this.todos))
+  }
+}
+
+Vue.createApp(todosApp).mount('#app')
+
